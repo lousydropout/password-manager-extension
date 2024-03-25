736ddb1abcd5f160db52e743961cd1caf4b56288
lousydropout <lousydropout@gmail.com>
2024-03-25 01:13:12 -0700

    add 'log out' function

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,9 +1,31 @@
 import { Box, Heading } from "@chakra-ui/react";
+import { CustomButton } from "./CustomButton";
 
 const Settings = () => {
   return (
-    <Box>
-      <Heading>Settings</Heading>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"space-between"}
+      minHeight={"100vh"}
+    >
+      <CustomButton
+        py={4}
+        colorScheme="warning"
+        onClick={async () => {
+          await chrome.storage.local.clear();
+          await chrome.storage.local.set({
+            context: JSON.stringify({
+              state: "CHECKING",
+              action: "",
+              context: {},
+              send: false,
+            }),
+          });
+        }}
+      >
+        Log out
+      </CustomButton>
     </Box>
   );
 };
